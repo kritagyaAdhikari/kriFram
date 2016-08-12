bfd7a901703a802994ab448d9d3caa16d8f57f25
kritagyaAdhikari <kritagya@gmail.com>
2016-08-12 17:57:03 +0545

    Update hello.ino

--- hello-world/hello.ino
+++ hello-world/hello.ino
@@ -1,42 +1,30 @@
-//include the framework
-#include "kriFram-0.h"
+
+#include "kriFram.h"
 
 void setup() {
   // initialize framework.
-  kritagyaSetup();
+  kriFramSetup();
 
-  
-//home page is not declared by framework app should make it
-server.on ( "/home", handleRoot );
-server.on ( "/", handleRoot );
+  Setting("call","Callibrate");
+  server.on ( "/home", handleRoot );
+  server.on ( "/", handleRoot );
 
-//add custom link on settings page
-addCustomSetting("call","Callibrate");
-//map custom setting page to function
-server.on("/call", handleCall );
-
-
-//start webServer
-server.begin();
-
+  server.begin();
 }
 
 // the loop function runs over and over again forever
 void loop() {
   //loop framework
-  kritagyaLoop();
+  kriFramLoop();
+
+
+
 }
 
 
 
 void handleRoot() {
-  String temp = "Hello World!";
-  server.send ( 200, "text/html", headder + temp + footer );
+  String body = "Hello World!";
+  server.send ( 200, "text/html", headder + body + footer );
   Serial.println ( "root page sent" );
-}
-
-void handleCall(){
-  String temp = "Hello World!";
-  server.send ( 200, "text/html", headder + temp + footer );
-  Serial.println ( "callibration page sent" );
 }
